e3de62acbdadc98cfd7817a08a8d1cb71eacd3b7
Lucas Murakami <lucaskaitomurakami@gmail.com>
2025-08-21 15:03:40 -0300

    fix: css on button feat: content projection card

--- src/app/home/home.ts
+++ src/app/home/home.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 import { WhosThatPokemon } from '../components/whos-that-pokemon/whos-that-pokemon';
 
 @Component({
   selector: 'app-home',
-  imports: [WhosThatPokemon],
+  imports: [WhosThatPokemon, RouterLink],
   templateUrl: './home.html',
   styleUrl: './home.css'
 })
--- src/app/witp-game/witp-game.ts
+++ src/app/witp-game/witp-game.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { WhosThatPokemon } from "../components/whos-that-pokemon/whos-that-pokemon";
 
 @Component({
   selector: 'app-witp-game',
-  imports: [],
+  imports: [WhosThatPokemon],
   templateUrl: './witp-game.html',
   styleUrl: './witp-game.css'
 })
