6270177ab1a55a2e532a19a8885534712ec7495a
Lucas Murakami <lucaskaitomurakami@gmail.com>
2025-08-21 22:55:33 -0300

    feat: fetch from the json and render on the html and fetch from the api first random pokemon

--- src/app/pages/witp-game/witp-game.ts
+++ src/app/pages/witp-game/witp-game.ts
@@ -1,12 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { WhosThatPokemon } from "../../components/whos-that-pokemon/whos-that-pokemon";
+import { Pokeapi } from '../../services/pokeapi.service';
+import { jsonPokemon, WhosThatPokemonCardInformation } from '../../models/pokeapi.model';
 
 @Component({
   selector: 'app-witp-game',
-  imports: [WhosThatPokemon],
+  imports: [WhosThatPokemon, CommonModule],
   templateUrl: './witp-game.html',
   styleUrl: './witp-game.css'
 })
-export class WitpGame {
+export class WitpGame implements OnInit {
+  pokemonRandomList: jsonPokemon[] = [];
+  pokemon: WhosThatPokemonCardInformation | null = null;
+  isLoading = true;
 
+  constructor(private pokeApi: Pokeapi) {}
+
+  ngOnInit(): void {
+      this.pokeApi.getRandomNames(4).subscribe(randomNames => {
+      this.pokemonRandomList = randomNames;
+      this.loadPokemonData(this.pokemonRandomList[0].name);
+    });
+  }
+
+  loadPokemonData(name: string): void {
+    this.isLoading = true;
+    this.pokeApi.getPokemonCardInfo(name).subscribe((data: WhosThatPokemonCardInformation) => {
+      this.pokemon = data;
+      this.isLoading = false;
+    }, error => {
+      console.error(error);
+      this.isLoading = false;
+    });
+  }
 }
--- src/app/services/pokeapi.service.ts
+++ src/app/services/pokeapi.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
-import { WhosThatPokemonCardInformation } from '../models/pokeapi.model';
+import { map, Observable } from 'rxjs';
+import { jsonPokemon, WhosThatPokemonCardInformation } from '../models/pokeapi.model';
 
 @Injectable({
   providedIn: 'root'
@@ -11,7 +11,19 @@ export class Pokeapi {
 
   constructor(private http: HttpClient) {}
 
+  // Gets the information of a specific Pokémon card.
   getPokemonCardInfo(name: string): Observable<WhosThatPokemonCardInformation> {
     return this.http.get<WhosThatPokemonCardInformation>(`${this.baseUrl}/pokemon/${name}`);
   }
+
+  // Gets from the Json random names. Takes as input a certain number to return the names.
+  getRandomNames(count: number): Observable<jsonPokemon[]> {
+  return this.http.get<jsonPokemon[]>('/json/pokedex_names.json').pipe(
+    map(names => {
+      const ids = Array.from({ length: 809 }, (_, i) => i + 1);
+      const randomIds = ids.sort(() => 0.5 - Math.random()).slice(0, count);
+      return randomIds.map(id => names[id - 1]);
+    })
+  );
+}
 }
